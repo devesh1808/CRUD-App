e79ae83873c16c47e7c2f8900ccf22b6a833753c
devesh1808 <devesh1808@gmail.com>
2023-10-13 14:48:23 +0530

    added some spacing between checkboxes

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -33,32 +33,40 @@ const Read = () => {
         />
       )}
       <h2 className="mt-4">All Users</h2>
-      <input
-        class="form-check-input"
-        name="gender"
-        checked={radioData === ""}
-        type="radio"
-        onChange={(e) => setRadioData("")}
-      />
-      <label class="form-check-label">All</label>
-      <input
-        class="form-check-input"
-        name="gender"
-        checked={radioData === "Male"}
-        value="Male"
-        type="radio"
-        onChange={(e) => setRadioData(e.target.value)}
-      />
-      <label class="form-check-label">Male</label>
-      <input
-        class="form-check-input"
-        name="gender"
-        value="Female"
-        checked={radioData === "Female"}
-        type="radio"
-        onChange={(e) => setRadioData(e.target.value)}
-      />
-      <label class="form-check-label">Female</label>
+      <div className="">
+        <span className="m-2">
+          <input
+            className="form-check-input"
+            name="gender"
+            checked={radioData === ""}
+            type="radio"
+            onChange={(e) => setRadioData("")}
+          />
+          <label className="form-check-label">All</label>
+        </span>
+        <span className="m-2">
+          <input
+            className="form-check-input"
+            name="gender"
+            checked={radioData === "Male"}
+            value="Male"
+            type="radio"
+            onChange={(e) => setRadioData(e.target.value)}
+          />
+          <label className="form-check-label">Male</label>
+        </span>
+        <span className="m-2">
+          <input
+            className="form-check-input"
+            name="gender"
+            value="Female"
+            checked={radioData === "Female"}
+            type="radio"
+            onChange={(e) => setRadioData(e.target.value)}
+          />
+          <label className="form-check-label">Female</label>
+        </span>
+      </div>
 
       <div className="row row-cols-md-3">
         {users &&
@@ -81,7 +89,10 @@ const Read = () => {
             })
 
             .map((ele) => (
-              <div key={ele.id} className="card  w-50 mx-auto my-2 border border-dark">
+              <div
+                key={ele.id}
+                className="card  w-50 mx-auto my-2 border border-dark"
+              >
                 <div className="card-body">
                   <h5 className="card-title">{ele.name}</h5>
                   <h6 className="card-subtitle mb-2 text-muted">{ele.email}</h6>
